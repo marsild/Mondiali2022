9428142ec3094b9afe92387a146b952549890d12
Author Name <marsild.spahiu@studio.unibo.it>
2022-07-11 10:08:06 +0200

    Create project and add icon asset

---
+++ Mondiali 2022/Mondiali 2022/Mondiali_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Mondiali_2022App.swift
+//  Mondiali 2022
+//
+//  Created by Marsild Spahiu on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Mondiali_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
