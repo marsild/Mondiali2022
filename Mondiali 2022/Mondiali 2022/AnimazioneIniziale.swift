56dd56d824db62069db35dc5ebc39e3909b90d43
Marsild Spahiu <marsild.spahiu@studio.unibo.it>
2022-08-27 09:20:30 +0200

    increasing 0.2 seconds launch animation

--- Mondiali 2022/Mondiali 2022/AnimazioneIniziale.swift
+++ Mondiali 2022/Mondiali 2022/AnimazioneIniziale.swift
@@ -23,7 +23,7 @@ struct AnimazioneIniziale: View {
                 }
             }.transition(.scale(scale: 100))
                 .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
                         withAnimation {
                             self.animazioneTerminata = true
                         }
